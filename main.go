4fb9e5d36695ccfea308bfc7831ac3191950bffa
peugeot <thomaspeugeot@yahoo.fr>
2024-06-05 03:13:18 +0200

    added comment

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 type Foo struct {
 }
 
+// Rename does not work for Foo
 // struct [Foo] is a nice struct ...
 
 func FuncA() {
 }
 
+// Rename works for FuncA
 // FuncB is same as [FuncA].
 func FuncB() {
 }
