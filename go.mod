57af01f07e6e9820022c1f65e22c0f6cd6236e70
peugeot <thomaspeugeot@yahoo.fr>
2024-06-05 03:09:34 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thomaspeugeot/issue_rename_57559
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+type Foo struct {
+}
+
+// struct [Foo] is a nice struct ...
+
+func FuncA() {
+}
+
+// FuncB is same as [FuncA].
+func FuncB() {
+}
+
+func main() {
+
+}
